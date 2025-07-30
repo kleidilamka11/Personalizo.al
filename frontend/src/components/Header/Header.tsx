37d8dbfcbd044bda2d1ba73a35ec398b7b71073f
kleidilamka11 <kleidilamka11@hotmail.com>
2025-07-30 17:04:41 +0200

    Add navigation menu to header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Container, Title } from './styles'
+import { Container, Title, NavMenu, NavItem } from './styles'
 
 const Header = () => {
   return (
     <Container>
       <Title>Personalizo.al</Title>
+      <NavMenu>
+        <NavItem to="/" end>
+          Home
+        </NavItem>
+        <NavItem to="/packages">Packages</NavItem>
+        <NavItem to="/orders">Orders</NavItem>
+        <NavItem to="/profile">Profile</NavItem>
+        <NavItem to="/login">Login</NavItem>
+        <NavItem to="/register">Register</NavItem>
+      </NavMenu>
     </Container>
   )
 }
